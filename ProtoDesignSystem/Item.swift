3e457e5c10c6589db1a0330e4bc6cf97b3198587
linnefromice <linnefromice@gmail.com>
2025-07-22 17:32:12 +0900

    initialize

---
+++ ProtoDesignSystem/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ProtoDesignSystem
+//
+//  Created by arata.haruyama on 2025/07/21.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ProtoDesignSystemTests/ProtoDesignSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoDesignSystemTests.swift
+//  ProtoDesignSystemTests
+//
+//  Created by arata.haruyama on 2025/07/21.
+//
+
+import Testing
+@testable import ProtoDesignSystem
+
+struct ProtoDesignSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
