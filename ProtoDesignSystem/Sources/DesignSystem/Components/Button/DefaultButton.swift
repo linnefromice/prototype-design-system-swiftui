dc48c3bc320761a468c3fc7b24eaf334e7e90f64
linnefromice <linnefromice@gmail.com>
2025-07-31 01:43:29 +0900

    update: btn

--- ProtoDesignSystem/Sources/DesignSystem/Components/Button/DefaultButton.swift
+++ ProtoDesignSystem/Sources/DesignSystem/Components/Button/DefaultButton.swift
@@ -1,24 +1,18 @@
 import SwiftUI
 
-struct Constants {
-    static let ColorPrimitiveBlue900: Color = Color(red: 0, green: 0.09, blue: 0.76)
-    static let BorderRadius8: CGFloat = 8
-}
-
 struct DefaultButton: View {
     let title: String
     let action: () -> Void
     
     var body: some View {
         Button(action: action) {
-            HStack(alignment: .center, spacing: 4) {
-                Text(title)
-                    .foregroundColor(.white)
-            }
-            .padding(16)
-            .background(Constants.ColorPrimitiveBlue900)
-            .cornerRadius(Constants.BorderRadius8)
+            Text(title)
+                .font(.system(size: 16, weight: .bold))
+                .foregroundColor(Color.Neutral.white)
         }
+        .padding(16)
+        .background(Color.Primitive.Blue.blue900)
+        .clipShape(RoundedRectangle(cornerRadius: Size.BorderRadius.val8))
     }
 }
 
